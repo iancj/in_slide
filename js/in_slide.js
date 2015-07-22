3a556255185614cdc13d41c6f8d02ad64c28cbd1
xiaozhe <xiaozhe@in66.com>
2015-07-22 14:10:30 +0800

    update

--- js/in_slide.js
+++ js/in_slide.js
@@ -4,6 +4,7 @@ $(function(){
 			$ul=$slide.find(".in-slide-list"),
 			$li=$slide.find("li"),
 			li_width=$li.offset().width,
+			li_margin=parseInt($li.css("margin-right")),
 			li_height=$li.height(),
 			li_size=$li.size(),
 			win_height=$(window).height(),
@@ -13,24 +14,72 @@ $(function(){
 			$slide.height(li_height);
 			$ul
 				.height(li_height)
-				.width((li_width+parseInt($li.css("margin-right")))*li_size)
+				.width((li_width+li_margin)*li_size)
 				.css("left",-li_width/2);
 
 			var start={},
 				delta={};
 
-			var curIndex=1;//当前展示的
+			var curIndex=1,
+				maxIndex=li_size-1,
+				prevIndex,
+				nextIndex,
+				beforePrevIndex,
+				afterNextIndex;
+
+			var $curDom,
+				$prevDom,
+				$nextDom,
+				$beforePrevDom,
+				$afterNextDom;
+
+			var requestAnimation=window.requestAnimationFrame ||
+								window.mozRequestAnimationFrame ||
+								window.webkitRequestAnimationFrame ||
+								window.msRequestAnimationFrame;
 
 			//初始化
 			function _setup(){
+				_updateIndex();
+
 				$li.each(function(index,item){
-					var scale= index==curIndex ? 1 : 0.88;
+					var scale;
+
+					switch(index){
+						case curIndex:
+							scale=1;
+							break;
+						case prevIndex:
+						case nextIndex:
+							scale=0.88;
+							break;
+						default:
+							scale=0.7;
+							break;
+					}
 
 					$(item).css({
 						"-webkit-transform":"translateX(0) scale("+scale+")",
-						"transform":"translateX(0) scale("+scale+")"
+						"transform":"translateX(0) scale("+scale+")",
+						"-webkit-transition":"all 0.3s linear",
+						"transition":"all 0.3s linear"
 					});
 				})
+			}
+
+			function _updateIndex(){
+				prevIndex=(curIndex-1)<0 ? maxIndex : curIndex-1,
+				nextIndex=(curIndex+1)>maxIndex ? maxIndex : curIndex+1,
+				beforePrevIndex=(prevIndex-1)<0 ? maxIndex : prevIndex-1,
+				afterNextIndex=(nextIndex+1)>maxIndex ? maxIndex : nextIndex+1;
+
+				$curDom=$li.eq(curIndex);
+				$prevDom=$li.eq(prevIndex);
+				$nextDom=$li.eq(nextIndex);
+				$beforePrevDom=$li.eq(beforePrevIndex);
+				$afterNextDom=$li.eq(afterNextIndex);
+
+				console.log(beforePrevIndex,prevIndex,curIndex,nextIndex,afterNextIndex)
 			}
 
 			_setup();
@@ -46,10 +95,72 @@ $(function(){
 						y:touch.pageY
 					}
 
+					$curDom.css({
+						"-webkit-transform":"translateX(0) scale(1)",
+						"transform":"translateX(0) scale(1)",
+					});
+
+					$prevDom.css({
+						"-webkit-transform":"translateX("+(-li_width)+"px) scale(0.88)",
+						"transform":"translateX("+(-li_width)+"px) scale(0.88)",
+					});
+
+					$nextDom.css({
+						"-webkit-transform":"translateX("+li_width+"px) scale(0.88)",
+						"transform":"translateX("+li_width+"px) scale(0.88)",
+					});
+
+					$beforePrevDom.css({
+						"-webkit-transform":"translateX("+(-li_width)*2+"px) scale(0.7)",
+						"transform":"translateX("+(-li_width)*2+"px) scale(0.7)",
+					});
+
+					$afterNextDom.css({
+						"-webkit-transform":"translateX("+li_width*2+"px) scale(0.7)",
+						"transform":"translateX("+li_width*2+"px) scale(0.7)",
+					});
+
 					$ul[0].addEventListener("touchmove",Events.move,false);
 					$ul[0].addEventListener("touchend",Events.end,false);
+				},
+				translate:function(dist){
+					if(dist<0){
+						var ratio=Math.abs(dist/win_height);
 
-					// console.log(start)
+						var cur_scale=1-ratio,
+							prev_scale=0.88-0.88*ratio,
+							next_scale=0.88+ratio,
+							afterNext_scale=0.7+0.88*ratio;
+
+						if(cur_scale<0.88) cur_scale=0.88;
+						if(prev_scale<0.7) prev_scale=0.7;
+						if(next_scale>1) next_scale=1;
+						if(afterNext_scale>0.88) afterNext_scale=0.88;
+
+						if(dist<0){
+							$curDom.css({
+								"-webkit-transform":"translateX("+dist+"px) scale("+cur_scale+")",
+								"transform":"translateX("+dist+"px) scale("+cur_scale+")"
+							});
+
+							$prevDom.css({
+								"-webkit-transform":"translateX("+dist+"px) scale("+prev_scale+")",
+								"transform":"translateX("+dist+"px) scale("+prev_scale+")"
+							});
+
+							$nextDom.css({
+								"-webkit-transform":"translateX("+dist+"px) scale("+next_scale+")",
+								"transform":"translateX("+dist+"px) scale("+next_scale+")"
+							});
+
+							if($curDom.offset().left<10){
+								$afterNextDom.css({
+									"-webkit-transform":"translateX("+dist+"px) scale("+afterNext_scale+")",
+									"transform":"translateX("+dist+"px) scale("+afterNext_scale+")"
+								});
+							}
+						}
+					}
 				},
 				move:function(e){
 					e.preventDefault();
@@ -61,60 +172,19 @@ $(function(){
 						y:touch.pageY-start.y
 					}
 
-					var $cur=$li.eq(1);
-
-					if(delta.x<0){
-						var ratio=Math.abs(delta.x/win_height),
-							curSize=li_size-1;
-							prevIndex=(curIndex-1)<0 ? curSize : curIndex-1,
-							nextIndex=(curIndex+1)>curSize ? curSize : curIndex+1;
-
-						var cur_scale=1-ratio,
-							prev_scale=0.88-0.88*ratio,
-							next_scale=0.88+ratio;
-
-						cur_scale=cur_scale<0.88 ? 0.88 : cur_scale;
-						prev_scale=prev_scale<0.8 ? 0.8 : prev_scale;
-						next_scale=next_scale>1 ? 1 : next_scale;
-
-						$li.eq(curIndex).css({
-							"-webkit-transform":"translateX("+delta.x+"px) scale("+cur_scale+")",
-							"transform":"translateX("+delta.x+"px) scale("+cur_scale+")"
-						});
-
-						$li.eq(prevIndex).css({
-							"-webkit-transform":"translateX("+delta.x+"px) scale("+prev_scale+")",
-							"transform":"translateX("+delta.x+"px) scale("+prev_scale+")"
-						});
-
-						$li.eq(nextIndex).css({
-							"-webkit-transform":"translateX("+delta.x+"px) scale("+next_scale+")",
-							"transform":"translateX("+delta.x+"px) scale("+next_scale+")"
-						});
-
-						// console.log($li[prevIndex],$li[curIndex],$li[NextIndex])
-
-					}
-
-
-					// $li.eq(1-1).css({
-					// 	"-webkit-transform":"translateX("+delta.x+"px) scale("+scale+")",
-					// 	"transform":"translateX("+delta.x+"px) scale("+scale+")"
-					// });
-
-					// $cur.css({
-					// 	"-webkit-transform":"translateX("+delta.x+"px)",
-					// 	"transform":"translateX("+delta.x+"px)"
-					// });
-
-					// $ul.css({
-					// 	"-webkit-transform":"translateX("+delta.x+"px)",
-					// 	"transform":"translateX("+delta.x+"px)"
-					// })
-
-					// console.log(delta)
+					requestAnimation(function(){
+						Events.translate(delta.x);
+					});
 				},
 				end:function(e){
+					e.preventDefault();
+
+					requestAnimation(function(){
+						Events.translate(-185);
+						curIndex=curIndex+1 > maxIndex? 0 : curIndex+1;
+						_updateIndex();
+					});
+
 					$ul[0].removeEventListener("touchmove",Events.move,false);
 					$ul[0].removeEventListener("touchend",Events.end,false);
 				}
